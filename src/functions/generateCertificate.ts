10c79b0184233e8009c43cb332ddc712c73aeb12
Thiago <tms1_tms2@hotmail.com>
2021-07-01 15:50:20 -0300

    Compile template

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,9 +1,31 @@
+import chromium from "chrome-aws-lambda";
+import path from "path";
+import handlebars from "handlebars";
+import fs from "fs";
+import dayjs from "dayjs";
+
 import { document } from "../utils/dynamodbClient";
 
 interface ICreateCertificate {
   id: string;
   name: string;
   grade: string;
+}
+
+interface ITemplate {
+  id: string;
+  name: string;
+  grade: string;
+  date: string;
+  medal: string;
+}
+
+const compile = async function(data: ITemplate) {
+  const filePath = path.join(process.cwd(), "src", "templates", "certificate.hbs");
+
+  const html = fs.readFileSync(filePath, "utf-8");
+
+  return handlebars.compile(html)(data);
 }
 
 export const handle = async (event) => {
@@ -13,6 +35,24 @@ export const handle = async (event) => {
     TableName: "users_certificates",
     Item: { id, name, grade }
   }).promise();
+
+  const medalPath = path.join(process.cwd(), "src", "templates", "selo.png");
+  const medal = fs.readFileSync(medalPath, "base64");
+
+  const data: ITemplate = {
+    date: dayjs().format("DD/MM/YYYY"),
+    grade,
+    name,
+    id,
+    medal
+  }
+  // Gerar certificado
+  // Compilar usando handlebars
+  const content = await compile(data)
+
+  // Transformar em pdf
+
+  // Salvar no S3
 
   return {
     statusCode: 201,
